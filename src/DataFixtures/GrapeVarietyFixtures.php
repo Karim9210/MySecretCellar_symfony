d3c741cd0289b6534663d7378be6a2b43b8b8033
Valerie <valerie.ielsch@gmail.com>
2022-11-23 13:50:27 +0100

    Entity grapeVAriety and fixture created

---
+++ src/DataFixtures/GrapeVarietyFixtures.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\GrapeVariety;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class GrapeVarietyFixtures extends Fixture
+{
+    public const GRAPEVARIETY =
+    [
+        "cabernet-franc noir",
+        "cabernet-sauvignon noir",
+        "carignan noir",
+        "chardonnay blanc",
+        "chenin blanc",
+        "gamay noir",
+        "gewurztraminer noir",
+        "gewurztraminer blanc",
+        "gewurztraminer rosé",
+        "grenache noir",
+        "merlot noir",
+        "muscat blanc",
+        "muscat noir",
+        "pinot noir",
+        "riesling blanc",
+        "sauvignon blanc",
+        "syrah noir",
+        "ugni blanc",
+        "viognier blanc",
+        "pinot blanc"
+    ];
+
+    public const PREFIX = 'grapeVariety_';
+    public function load(ObjectManager $manager): void
+    {
+        foreach (self::GRAPEVARIETY as $grapeVarietyLabel) {
+            $grapeVariety = new GrapeVariety();
+            $grapeVariety->setLabel($grapeVarietyLabel);
+            $manager->persist($grapeVariety);
+            $this->addReference(self::PREFIX . $grapeVarietyLabel, $grapeVariety);
+
+            $manager->flush();
+        }
+    }
+}
