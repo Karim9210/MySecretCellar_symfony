8933aa1e6676b11aa76bfa7280821bd5b5c5c852
laura vicente <lauravic.pro@gmail.com>
2022-11-25 11:38:04 +0100

    grump fixing

--- src/Repository/WineRepository.php
+++ src/Repository/WineRepository.php
@@ -15,7 +15,6 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method Wine[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class WineRepository extends ServiceEntityRepository
-
 {
     public const TABLE = 'wine';
 
@@ -42,7 +41,7 @@ class WineRepository extends ServiceEntityRepository
         }
     }
 
-   
+
         // public function Stock()
         // {
 
