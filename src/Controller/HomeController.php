52c9506e60691c799787b07d2b46b79673c0f541
Valerie <valerie.ielsch@gmail.com>
2022-11-30 15:35:22 +0100

    securisation des routes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,13 +3,15 @@
 namespace App\Controller;
 
 use App\Repository\WineRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
+    #[IsGranted('ROLE_USER')]
     public function index(WineRepository $wineRepository): Response
     {
 
